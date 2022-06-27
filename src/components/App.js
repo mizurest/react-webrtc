7b13d96af8cc62c0bcd5cd1e40636dae42f4baaf
k <c0p31034@bunkyo.ac.jp>
2022-06-27 20:56:37 +0900

    サインイン画面の作成

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import InputFormLocal from './InputFormLocal';
+import InputFormRemote from './InputFormRemote';
+
+const getMedia = async () => {
+  const constraints = {audio: true, video: true} //　要求するメディアの種類を指定
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    // ストリームを使用
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+// ユーザーにメディアの使用許可を求める
+getMedia();
+
+const App = () => {
+  const [localName, setLocalName] = useState('') // 自分の名前を保持するState
+  const [remoteName, setRemoteName] = useState('') // 相手の名前を保持するState
+  return (
+    <>
+      <InputFormLocal
+        localName={localName}
+        setLocalName={setLocalName}
+      />
+      <InputFormRemote
+        remoteName={remoteName}
+        setRemoteName={setRemoteName}
+      />
+    </>
+  )
+}
+
+export default App;
--- src/components/InputFormLocal.js
+++ src/components/InputFormLocal.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
@@ -17,8 +15,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/mizurest">
+        Mizurest
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -57,7 +55,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignInSide() {
+const InputFormLocal = () => {
   const classes = useStyles();
 
   return (
@@ -70,7 +68,7 @@ export default function SignInSide() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            名前を入力してください
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
@@ -78,26 +76,10 @@ export default function SignInSide() {
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
+              label="名前"
               name="email"
-              autoComplete="email"
+              autoComplete="name"
               autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
             />
             <Button
               type="submit"
@@ -106,20 +88,8 @@ export default function SignInSide() {
               color="primary"
               className={classes.submit}
             >
-              Sign In
+              登録
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
             <Box mt={5}>
               <Copyright />
             </Box>
@@ -129,3 +99,5 @@ export default function SignInSide() {
     </Grid>
   );
 }
+
+export default InputFormLocal;
