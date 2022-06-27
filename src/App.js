94ab75b624fb8220294c3f300990005b679db1f9
k <c0p31034@bunkyo.ac.jp>
2022-06-27 16:53:29 +0900

    ユーザーにメディアの使用許可を求める

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-function App() {
+const getMedia = async () => {
+  const constraints = {audio: true, video: true} //　要求するメディアの種類を指定
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    // ストリームを使用
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+// ユーザーにメディアの使用許可を求める
+getMedia();
+
+const App = () => {
   return <div>Hello, React!</div>;
 }
 
