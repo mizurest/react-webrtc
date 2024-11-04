2fd34c208667e0135f8a887992e7820a399f9456
k <c0p31034@bunkyo.ac.jp>
2024-11-05 04:03:52 +0900

    update config

--- src/utils/FirebaseSignalingClient.js
+++ src/utils/FirebaseSignalingClient.js
@@ -4,13 +4,13 @@ import 'firebase/database';
 export default class FirebaseSignalingClient {
     constructor() {
         const firebaseConfig = {
-            apiKey: "AIzaSyBZFrvWj_WkDZHGJkkSuErwUCGOHORaU5A",
-            authDomain: "react-webrtc-64850.firebaseapp.com",
-            databaseURL: "https://react-webrtc-64850-default-rtdb.firebaseio.com",
-            projectId: "react-webrtc-64850",
-            storageBucket: "react-webrtc-64850.appspot.com",
-            messagingSenderId: "705715137289",
-            appId: "1:705715137289:web:ab00749a451e249f7f81d8"
+            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
+            appId: process.env.REACT_APP_FIREBASE_APP_ID
         };
         if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
         this.database = firebase.database();
